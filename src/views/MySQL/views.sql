c2c3472ce4afce4f7769ad78f408aa3fe084d979
MassiGy <ghernaoutmassi@gmail.com>
2023-12-04 10:26:55 +0100

    +dev-massigy-part4: moved the views files

---
+++ src/views/MySQL/views.sql
@@ -0,0 +1,36 @@
+-- CREATION DES VUES
+
+-- Cette vue donne les informations sur les collectionneurs associé à leur nombre de pièces
+CREATE OR REPLACE VIEW P06_CollectionneursInfos AS
+    SELECT C.*, SUM(QteCollection) AS NbPiece
+    FROM P06_Collectionneur AS C
+    LEFT JOIN P06_Collectionner
+    ON C.CollectionneurID = P06_Collectionner.CollectionneurID
+    GROUP BY C.CollectionneurID;
+
+-- SELECT * FROM P06_CollectionneursInfos ;
+
+
+-- Cette vue donne les modeles de pieces qui ne sont pas en possession d'un collectionneur.
+CREATE OR REPLACE VIEW P06_PiecesNonPossedees AS
+    SELECT *
+    FROM P06_PieceModele
+    WHERE PieceID NOT IN (
+        SELECT P06_Collectionner.PieceID
+        FROM P06_Collectionner
+        );
+
+
+-- SELECT * FROM P06_PiecesNonPossedees ;
+
+-- Cette vue donne toutes les informations d'une pièce
+CREATE OR REPLACE VIEW P06_PieceInfos AS
+    SELECT PM.*, PC.PieceFaceCommune, PC.PieceMasse, PC.PieceTaille, PC.PieceMateriau, PT.PieceTranche, PP.PaysNom
+        FROM P06_PieceModele PM
+        INNER JOIN P06_PieceCaracteristique PC ON PM.PieceID = PC.PieceID
+        INNER JOIN P06_PieceTranche PT ON PM.PieceID = PT.PieceID
+        INNER JOIN P06_PiecePays PP ON PM.PieceID = PP.PieceID
+    ORDER BY PM.PieceID;
+
+-- SELECT * FROM P06_PieceInfos ;
+
