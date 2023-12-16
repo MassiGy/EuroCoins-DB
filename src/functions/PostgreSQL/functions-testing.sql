d7b50c1a0a3e8e660b76b6193490a870d337f443
MassiGy <ghernaoutmassi@gmail.com>
2023-12-16 16:41:44 +0100

    +dev-massigy-testing: added some functions to practice

--- src/functions/PostgreSQL/functions-testing.sql
+++ src/functions/PostgreSQL/functions-testing.sql
@@ -91,3 +91,97 @@ end;
 $$ language plpgsql;
 
 
+
+-- créer une fonction qui utilise une cursor et une 
+-- boucle.
+-- pour cela, on va crée une fonction qui va afficher
+-- le nom et le prénom de chaque collectionneur avec
+-- une pièce très rare (émise pour un championat ou un 
+-- grand anniversaire).
+-- on va faire que de l'affichage donc c'est une procedure
+create or replace procedure collectionneur_possedant_pieces_rare() as $$
+declare 
+    row p06_collectionneur%rowtype;
+    curseur cursor for 
+            select * 
+            from p06_collectionneur 
+            where collectionneurid in (
+                select collectionneurid 
+                from p06_collectionner
+                where pieceid in (
+                    select pieceid 
+                    from p06_piecemodele
+                    where pieceversion like '%Championat%'
+                    or pieceversion like '%anniversaire%'
+                 )
+            );
+begin
+    for row in curseur 
+    loop 
+        raise notice 'M/Mme % % possède queqlues pieces rares!', row.collectionneurprenom, row.collectionneurnom;
+    end loop;
+end;
+$$ language plpgsql;
+
+
+-- créer une fonction qui va retourner une sous 
+-- partie d'une table 
+create or replace function 
+    filtrer_piece_par_valeurs(valeur int)
+returns setof p06_piecemodele as $$ 
+declare 
+    curseur cursor(val int) for
+            select * from 
+            p06_piecemodele 
+            where piecevaleur = val;
+    row p06_piecemodele%rowtype;
+begin
+    for row in curseur(valeur) 
+    loop 
+        -- queue (add one to queue)
+        return next row;
+    end loop;
+    
+    -- flush (return all)
+    return;  
+end;
+$$ language plpgsql; 
+
+
+
+-- créer une procedure fizzbuzz (ce ci n'a aucun rapport avec le projet)
+create or replace procedure
+   fizzbuzz(range int) 
+as $$
+declare 
+    i int;
+begin
+    for i in 1 .. range
+    loop
+        if mod(i,15)=0 
+        then 
+            raise notice 'fizzbuzz';
+        elsif mod(i, 5)=0 
+        then 
+            raise notice 'buzz';
+        elsif mod(i, 3)=0
+        then    
+            raise notice 'fizz';
+        else
+            raise notice '%',i; 
+        end if; 
+    end loop;
+
+end;
+$$ language plpgsql;
+
+
+
+
+
+
+
+
+
+
+
